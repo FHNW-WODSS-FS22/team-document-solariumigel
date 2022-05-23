ecdb80d554938a3858837ebbf89777863f8f87e6
joshschaerer <joshua.schaerer@students.fhnw.ch>
2022-05-23 15:19:09 +0200

    removed all warnings which led to the pipeline failing

--- Client/react_client/src/pages/DoctumentEditor.js
+++ Client/react_client/src/pages/DoctumentEditor.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { HubConnectionState } from "@microsoft/signalr";
-import { Link, useParams  } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ParagraphList from "../components/ParagraphList";
 import Paragraph from "../components/Paragraph";
 
@@ -11,9 +11,6 @@ export default function DoctumentEditor(props) {
   const { api, connectionBuilder, documentProvider, userProvider } = props;
   const [connection, setConnection] = useState();
   const [document, setDocument] = useState();
-  const [paragraphs, setParagraphs] = useState();
-  const [user, setUser] = useState();
-  const [documentUsers, setDocumentUsers] = useState([]);
   const { id } = useParams();
 
   /**
@@ -21,42 +18,46 @@ export default function DoctumentEditor(props) {
    */
   useEffect(() => {
     setConnection(connectionBuilder.buildConnection());
-  }, []);
-
-  useEffect(()  => {
-    if(api)
-    {
-      api.getDocument(id).then(doc => {
-        documentProvider.setDocument(doc)
-        setDocument(doc)
-      });
-    }
-  }, [api]);
+  }, [connectionBuilder]);
 
   useEffect(() => {
+    if (api) {
+      api.getDocument(id).then((doc) => {
+        documentProvider.setDocument(doc);
+        setDocument(doc);
+      });
+    }
+  }, [api, documentProvider, id]);
 
-    if(connection && connection.state !== HubConnectionState.Connected)
-    {
+  useEffect(() => {
+    if (connection && connection.state !== HubConnectionState.Connected) {
       connection.on("SetCurrentUser", setCurrentUser);
       connection.on("SetUserId", setUserId);
-      connection.on("UnSetUserId", unSetUserId); 
+      connection.on("UnSetUserId", unSetUserId);
 
       connection.start().then(() => {
-        if(userProvider.getUser()){
-          connection.send("AddToDocumentWithUser", documentProvider.getDocument().id, userProvider.getUser());
-        }
-        else{
+        if (userProvider.getUser()) {
+          connection.send(
+            "AddToDocumentWithUser",
+            documentProvider.getDocument().id,
+            userProvider.getUser()
+          );
+        } else {
           connection.send("AddToDocument", documentProvider.getDocument().id);
-        }});   
+        }
+      });
     }
-  }, [document]);
-
+  });
 
   /**
    * Create a new paragraph
    */
   const createParagraph = () => {
-    connection.send("CreateParagraph", documentProvider.getDocument().id, userProvider.getUser());
+    connection.send(
+      "CreateParagraph",
+      documentProvider.getDocument().id,
+      userProvider.getUser()
+    );
   };
 
   /**
@@ -65,58 +66,62 @@ export default function DoctumentEditor(props) {
    */
   const setCurrentUser = (user) => {
     userProvider.setUser(user);
-    documentProvider.setDocumentUser([user])
-    setUser(user);
+    documentProvider.setDocumentUser([user]);
   };
 
   const setUserId = (user) => {
     documentProvider.addUser(user);
-    setDocumentUsers(documentProvider.getUsers());
-  }
+  };
 
   const unSetUserId = (user) => {
     documentProvider.removeUser(user);
-    setDocumentUsers(documentProvider.getUsers());
-  }
+  };
 
   /**
- * Delete a paragraph
- * @param {object} paragraphId
- */
+   * Delete a paragraph
+   * @param {object} paragraphId
+   */
   const deleteParagraph = (paragraphId) => {
     connection.send("DeleteParagraph", document.id, paragraphId);
   };
 
   const ConverToItems = () => {
-    if(documentProvider.hasParagraphs() == false)
-    {
+    if (documentProvider.hasParagraphs() === false) {
       return;
     }
-    const sorted = documentProvider.getParagraphs().sort((a, b) => { return a.position > b.position ? 1 : -1});
-    const sortedItems = sorted.map(paragraph => {
-      return <Paragraph
-        connection={connection}
-        documentId={document.id}
-        paragraph={paragraph}
-        text={paragraph.text}
-        position={paragraph.position}
-        userProvider={userProvider}
-        documentProvider={documentProvider}
-        key={paragraph.id}
-        onDelete={deleteParagraph}
-      />
-    })
-    
-    documentProvider.setParagraphItems(sortedItems)
+    const sorted = documentProvider.getParagraphs().sort((a, b) => {
+      return a.position > b.position ? 1 : -1;
+    });
+    const sortedItems = sorted.map((paragraph) => {
+      return (
+        <Paragraph
+          connection={connection}
+          documentId={document.id}
+          paragraph={paragraph}
+          text={paragraph.text}
+          position={paragraph.position}
+          userProvider={userProvider}
+          documentProvider={documentProvider}
+          key={paragraph.id}
+          onDelete={deleteParagraph}
+        />
+      );
+    });
+
+    documentProvider.setParagraphItems(sortedItems);
     return sortedItems;
-  }
+  };
 
   const NavigateBack = () => {
-    connection.send("RemoveFromDocument", documentProvider.getDocument().id, userProvider.getUser());
-    documentProvider.setDocumentUser([])
+    connection.send(
+      "RemoveFromDocument",
+      documentProvider.getDocument().id,
+      userProvider.getUser()
+    );
+    documentProvider.setDocumentUser([]);
     connection.off();
     connection.stop();
-  }
+  };
 
   return (
     <div>
@@ -124,16 +129,28 @@ export default function DoctumentEditor(props) {
         <div>
           <div className="top">
             <div className="topLeft">
-              <Link className="backArrow" onClick={NavigateBack} to={"/"}></Link>
+              <Link
+                className="backArrow"
+                onClick={NavigateBack}
+                to={"/"}
+              ></Link>
               <div className="documentTitle">{document.name}</div>
-              <button className="addParagraph" onClick={() => createParagraph()}></button>
+              <button
+                className="addParagraph"
+                onClick={() => createParagraph()}
+              ></button>
             </div>
             <div className="rightTop">
               <p>Document Owner: {document.owner}</p>
               <p className="userName">User: {userProvider.getUser()}</p>
             </div>
           </div>
-          <div className="userLeft"><ul>{documentProvider.getUsers() && documentProvider.getUsers().map((u) => <p key={u}>{u}</p>)}</ul></div>
+          <div className="userLeft">
+            <ul>
+              {documentProvider.getUsers() &&
+                documentProvider.getUsers().map((u) => <p key={u}>{u}</p>)}
+            </ul>
+          </div>
           <div className={Math.random()}>
             <ParagraphList
               connection={connection}
