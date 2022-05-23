e501123c79a59cea5a851962deee8555b117fd92
TimonKeller <55238597+TimonKeller@users.noreply.github.com>
2022-05-23 21:31:55 +0200

    Update Paragraph.test.js

--- Client/react_client/src/components/Paragraph.test.js
+++ Client/react_client/src/components/Paragraph.test.js
@@ -1,18 +1,28 @@
 import { render, screen, fireEvent} from '@testing-library/react';
 import Paragraph from './Paragraph';
 import { HubConnection } from '@microsoft/signalr';
+import UserProvider from '../helpers/UserProvider';
 jest.mock('@microsoft/signalr');
 
 const hubConnection = new HubConnection();
+const userProvider = new UserProvider();
 
 const mockedOnDelete = jest.fn();
 const mockedUpdateText = jest.fn();
 const mockedListenForText = jest.fn();
+const mockedGetUser = jest.fn();
 
 const testParagraph = {
     id: 123456,
-    owner: 'Peter'
+    owner: 'Peter',
+    position: 1
 };
+
+const testDocument = {
+    id: 987676
+};
+
+
 
 test('paragraph owner is correct', () => { 
     render(<Paragraph paragraph={testParagraph}/>);
@@ -36,9 +46,39 @@ test('delete paragraph with btn', async() => {
 })
 
 test('textarea can be edited', async() => {
-    render(<Paragraph paragraph={testParagraph} onChange={mockedUpdateText} 
-        connection={hubConnection} on={("", mockedListenForText)}/>);
+    render(<Paragraph paragraph={testParagraph} onChange={mockedUpdateText} connection={hubConnection} on={("", mockedListenForText)} />);
     const inputElement = screen.getByTestId('paragraph-textarea');
     fireEvent.change(inputElement, {target: {value: "Test Eintrag"}});
     expect(inputElement.value).toBe("Test Eintrag");
+})
+
+test('test onClick textArea', async() => {
+    render(<Paragraph paragraph={testParagraph} onChange={mockedUpdateText} connection={hubConnection} 
+        on={("", mockedListenForText)} send={("", testDocument.id, testParagraph.id, userProvider)} userProvider={userProvider} getUser={mockedGetUser}/>);
+    const inputElement = screen.getByTestId('paragraph-textarea');
+    fireEvent.click(inputElement);
+    expect(testDocument.id).toBe(987676);
+    expect(testParagraph.id).toBe(123456);
+})
+
+test('arrow up can be clicked', async() => {
+    render(<Paragraph paragraph={testParagraph} connection={hubConnection} send={("", testDocument.id, testParagraph.id)} />);
+    const arrowUpButton = screen.getByTestId('arrowup-btn');
+    fireEvent.click(arrowUpButton);
+    expect(testParagraph.id).toBe(123456);
+    expect(testDocument.id).toBe(987676);
+})
+
+test('paragraph position', async() => {
+    render(<Paragraph paragraph={testParagraph} position={testParagraph.position}/>);
+    const positionElement = screen.getByTestId('paragraph-position');
+    expect(positionElement.textContent).toBe("1");
+  })
+
+test('arrow down can be clicked', async() => {
+    render(<Paragraph paragraph={testParagraph} connection={hubConnection} send={("", testDocument.id, testParagraph.id)} />);
+    const arrwoDownButton = screen.getByTestId('arrowdown-btn');
+    fireEvent.click(arrwoDownButton);
+    expect(testParagraph.id).toBe(123456);
+    expect(testDocument.id).toBe(987676);
 })
